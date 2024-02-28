a02c66840cb1f5f7c29bdf11f1e41344d672cb31
Kattsyn <warshard1337@gmail.com>
2024-02-28 17:07:15 +0300

    added task1 solution

--- task1.py
+++ task1.py
@@ -1,14 +1,41 @@
 import random
 
+
+def create_random_list(amount_of_elems):
+    list = []
+    for i in range(amount_of_elems):
+        list[i].append(random.randint(-10, 10))
+    return list
+
+
+def get_list_from_file(path):
+    file = open(path, 'r')
+    line = file.read()
+    list = line.split(" ")
+    for i in range(len(list)):
+        list[i] = int(list[i])
+    file.close()
+    return list
+
+def write_list_to_file(path, list):
+    file = open(path, 'w')
+    for elem in list:
+        file.write(str(elem) + " ")
+    file.close()
+
+
 def solution(list):
     for i in range(len(list)):
         for j in range(len(list) - 1):
             if list[j] >= 0 and list[j + 1] < 0:
                 list[j], list[j + 1] = list[j + 1], list[j]
     return list
-list = []
-for i in range(10):
-    list.append(random.randint(-10,10))
 
-print(list)
-print(solution(list))
+
+list = get_list_from_file('task1_input.txt')
+
+list = solution(list)
+
+write_list_to_file('task1_output.txt', list)
+
+
