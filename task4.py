e072e88715bd9fb17f9fa7097e9e118161924d44
Kattsyn <vov4ikkat3@mail.ru>
2024-03-13 00:52:55 +0300

    fixed bugs

--- task4.py
+++ task4.py
@@ -1,6 +1,8 @@
 def number_to_words(num):
     units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
-    teens = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+    units_2 = ['', 'одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+    teens = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать',
+             'семнадцать', 'восемнадцать', 'девятнадцать']
     tens = ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
     hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
     thousands = ['', 'тысяча', 'тысячи', 'тысяч']
@@ -21,19 +23,22 @@ def number_to_words(num):
             result += teens[thousands_part - 10] + ' '
         else:
             result += tens[thousands_part // 10] + ' '
-            result += units[thousands_part % 10] + ' '
-        if num % 1000 == 0:
-            result += thousands[0] + ' '
-        elif num % 1000 in range(10, 20):
+            if num < 10000:
+                result += units_2[thousands_part % 10] + ' '
+            else:
+                result += units[thousands_part % 10] + ' '
+
+        if thousands_part % 100 in range(10, 20):
             result += thousands[3] + ' '
-        elif num % 1000 % 100 in range(10, 20):
-            result += thousands[3] + ' '
-        elif num % 1000 % 10 == 1:
-            result += thousands[1] + ' '
-        elif num % 1000 % 10 in range(2, 5):
-            result += thousands[2] + ' '
         else:
-            result += thousands[3] + ' '
+            if thousands_part % 10 == 0:
+                result += thousands[3] + ' '
+            elif thousands_part % 10 == 1:
+                result += thousands[1] + ' '
+            elif thousands_part % 10 in range(2, 5):
+                result += thousands[2] + ' '
+            elif thousands_part % 10 in range(5, 10):
+                result += thousands[3] + ' '
         num %= 1000
 
     if num // 100 > 0:
@@ -47,5 +52,22 @@ def number_to_words(num):
 
     return result.strip()
 
+
 # Пример использования
-print(number_to_words(217045))  # Двести семнадцать тысяч сорок пять
+print(number_to_words(217045))  # двести семнадцать тысяч сорок пять
+print(number_to_words(123456))  # сто двадцать три тысячи четыреста пятьдесят шесть
+print(number_to_words(123000))  # сто двадцать три тысячи
+print(number_to_words(123005))  # сто двадцать три тысячи  пять
+print(number_to_words(123015))  # сто двадцать три тысячи пятнадцать
+print(number_to_words(12115))  # двенадцать тысяч сто пятнадцать
+print(number_to_words(2115))  # две тысячи сто пятнадцать
+print(number_to_words(456))  # четыреста пятьдесят шесть
+print(number_to_words(13))  # тринадцать
+print(number_to_words(1))  # один
+print(number_to_words(44444))  # сорок четыре тысячи четыреста сорок четыре
+print(number_to_words(10001))  # десять тысяч  один
+print(number_to_words(194555))  # сто девяносто четыре тысячи пятьсот пятьдесят пять
+print(number_to_words(10101))  # десять тысяч сто  один
+
+# for i in range(1000000):
+#     print(number_to_words(i))
