26cded129ec774c2704e04bd8e7060eae23a4fa7
Kattsyn <warshard1337@gmail.com>
2024-05-29 17:17:16 +0300

    changed 3-4 tasks

--- task3/Circle.py
+++ task3/Circle.py
@@ -1,9 +1,4 @@
-import math
-
-
 class Circle:
-    """Класс для представления круга с координатами центра и радиусом."""
-
     def __init__(self, x, y, radius):
         self.center = (x, y)
         self.radius = radius
